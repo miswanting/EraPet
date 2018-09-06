80a1dc017178b52ac35634d13fc5eff6552b5963
何雨航 <453542772@qq.com>
2018-09-06 20:13:33 +0800

    Go

---
+++ !DEBUG.bat
@@ -0,0 +1,3 @@
+@echo off
+python Script.py
+pause
---
+++ Script.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+import erajs.era as e
+
+
+# def start_new_game():
+#     e.page()
+#     e.h('玩家角色创建方式')
+#     e.t()
+#     e.t()
+#     e.b('使用默认主角', e.goto, default_person)
+
+
+# def default_person():
+#     e.page()
+#     e.h('默认玩家角色')
+#     e.t()
+#     e.t()
+
+
+e.init()
+# e.title('EraPet')
+# e.h('EraPet')
+# e.t()
+# e.t()
+# e.b('开始饲养', e.goto, start_new_game)
+# e.t()
+# e.b('继续饲养', None)
