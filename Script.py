839c9a766e1bd823a7ebdb96f24f1bf0f2193aa1
何雨航 <453542772@qq.com>
2018-09-18 22:09:26 +0800

    Enable Test Script.

--- Script.py
+++ Script.py
@@ -2,26 +2,26 @@
 import erajs.api as a
 
 
-# def start_new_game():
-#     e.page()
-#     e.h('玩家角色创建方式')
-#     e.t()
-#     e.t()
-#     e.b('使用默认主角', e.goto, default_person)
+def start_new_game():
+    a.page()
+    a.h('玩家角色创建方式')
+    a.t()
+    a.t()
+    a.b('使用默认主角', a.goto, default_person)
 
 
-# def default_person():
-#     e.page()
-#     e.h('默认玩家角色')
-#     e.t()
-#     e.t()
+def default_person():
+    a.page()
+    a.h('默认玩家角色')
+    a.t()
+    a.t()
 
 
 a.init()
 a.title('EraPet')
-# e.h('EraPet')
-# e.t()
-# e.t()
-# e.b('开始饲养', e.goto, start_new_game)
-# e.t()
-# e.b('继续饲养', None)
+a.h('EraPet')
+a.t()
+a.t()
+a.b('开始饲养', a.goto, start_new_game)
+a.t()
+a.b('继续饲养', None)
